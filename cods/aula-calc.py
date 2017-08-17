65ebaff7d9dfaae81b643e6849c757b7e8d46d5c
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-08-17 10:39:14 -0300

    adding expo function to the calc

--- cods/aula-calc.py
+++ cods/aula-calc.py
@@ -5,15 +5,19 @@ lexer = ox.make_lexer([
     ('NUMBER', r'\d+(\.\d*)?'),
     ('OP_S', r'[-+]'),
     ('OP_M', r'[*/]'),
+    ('OP_E', r'\^'),
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
 ])
 
 infix =  lambda x, op, y: (op, x, y)
 atom = lambda x: ('atom', float(x))
-tokens_list = ['NUMBER', 'OP_S', 'OP_M']
+tokens_list = ['NUMBER', 'OP_S', 'OP_M', 'OP_E', 'LPAREN', 'RPAREN']
 
 parser = ox.make_parser([
     ('expr : expr OP_S term', infix),
     ('expr : term', lambda x: x),
+    ('term : term OP_E atom', infix),
     ('term : term OP_M atom', infix),
     ('term : atom', lambda x: x),
     ('atom : NUMBER', atom),
@@ -24,13 +28,14 @@ OP_TO_FUNC = {
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
     '/': lambda x, y: x / y,
+    '^': lambda x, y: x ** y,
 }
 
 def eval(ast):
     head, *tail = ast
     if head == 'atom':
         return tail[0]
-    elif head in {'+', '-', '*', '/'}:
+    elif head in {'+', '-', '*', '/', '^'}:
         func = OP_TO_FUNC[head]
         x, y = map(eval, tail)
         return func(x, y)
